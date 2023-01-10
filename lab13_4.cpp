1b24d5fe75c0382244d0919a07a15475a5d19358
= <=>
2023-01-10 16:22:14 +0700

    lab13_1_completed

--- lab13_4.cpp
+++ lab13_4.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 
 using namespace std;
+template <typename stpairs >
+void mySwap(stpairs &x,stpairs &y) ;
+
 
 int main(){
 	int x, y;
@@ -29,4 +32,10 @@ int main(){
 
 	return 0;
 }
-
+template <typename stpairs >
+void  mySwap(stpairs &x,stpairs &y)
+{
+	stpairs z= x ;
+	 x= y ;
+	 y = z ;
+}
